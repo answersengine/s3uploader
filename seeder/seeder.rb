a2dbb2bec8e430612b5152208ee2d9ea1f799496
Parama Danoesubroto <parama@answersengine.com>
2019-09-04 14:27:59 -0400

    added directory loop

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,16 +1,13 @@
 # directories = `s3cmd ls #{ENV['AWS_S3_BUCKET_NAME']}`
-directories = %x(s3cmd ls #{ENV['AWS_S3_BUCKET_NAME']})
+directories = %x(s3cmd ls #{ENV['AWS_S3_BUCKET_NAME']}).split.
+directories.delete_if {|i| i == "DIR"}
 # directories = %x(echo )
 # directories = ""
 #puts "directories: #{directories}"
-
-outputs << {
+directories.each do |dir|
+  outputs << {
     _collection: 'directories',
-    _id: 'directories',
-    directories: directories,
-    bucket: ENV['AWS_S3_BUCKET_NAME'],
-    access_key: ENV['AWS_ACCESS_KEY_ID'],
-    access_secret: ENV['AWS_SECRET_ACCESS_KEY'],
-    access_key_echo: `echo $AWS_ACCESS_KEY_ID`,
-    access_secret_echo: `echo $AWS_SECRET_ACCESS_KEY`
+    _id: dir,
+    directory: dir,
   }
+end
