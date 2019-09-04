11a97612f547fb6ed6e03d7dd58f21c8128f6a1b
Parama Danoesubroto <parama@answersengine.com>
2019-09-04 14:11:35 -0400

    added echo

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,4 +1,6 @@
-#directories = `s3cmd ls #{ENV['AWS_S3_BUCKET_NAME']}`
+# directories = `s3cmd ls #{ENV['AWS_S3_BUCKET_NAME']}`
+# directories = %x(s3cmd ls #{ENV['AWS_S3_BUCKET_NAME']})
+# directories = %x(echo )
 directories = ""
 #puts "directories: #{directories}"
 
@@ -8,6 +10,7 @@ outputs << {
     directories: directories,
     bucket: ENV['AWS_S3_BUCKET_NAME'],
     access_key: ENV['AWS_ACCESS_KEY_ID'],
-    access_secret: ENV['AWS_SECRET_ACCESS_KEY']#,
-    #access_key_echo: `echo $AWS_ACCESS_KEY_ID`
+    access_secret: ENV['AWS_SECRET_ACCESS_KEY'],
+    access_key_echo: `echo $AWS_ACCESS_KEY_ID`,
+    access_secret_echo: `echo $AWS_SECRET_ACCESS_KEY`
   }
