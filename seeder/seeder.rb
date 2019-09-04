7521abae7d99f6f4de349ed378a7a60059c7574d
Parama Danoesubroto <parama@answersengine.com>
2019-09-04 13:29:04 -0400

    another fix

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -8,6 +8,6 @@ outputs << {
     directories: directories,
     bucket: ENV['AWS_S3_BUCKET_NAME'],
     access_key: ENV['AWS_ACCESS_KEY_ID'],
-    access_secret: ENV['AWS_SECRET_ACCESS_KEY'],
-    access_key_echo: `echo $AWS_ACCESS_KEY_ID`
+    access_secret: ENV['AWS_SECRET_ACCESS_KEY']#,
+    #access_key_echo: `echo $AWS_ACCESS_KEY_ID`
   }
